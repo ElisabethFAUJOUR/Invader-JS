9205df849671f8d21ba477dc0f0b07c2ffa05936
Elisabeth Faujour <efaujour@gmail.com>
2023-05-25 15:19:41 +0200

    Etape 3 création du l'input et du bouton

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 //ETAPE 1 = Générer la grille de pixel 
 
 /*Selection de la div id=invader*/
-const invaderDiv = document.getElementById('invader');
+const invaderDiv = document.getElementById("invader");
 
 
 /*Création de la grille avec les 8 * 8 pixels*/
 for (let i = 0; i < 64; i++) { /*Boucle pour générer les 64 pixels*/
-  const cellDiv = document.createElement('div'); /*Création de la div pour créer un pixel*/
-  cellDiv.classList.add('cell'); /*Ajouter une classe "cell" sur cette div*/
-  cellDiv.style.backgroundColor = 'rgb(210, 218, 226)'; /*Ajouter un background gris/ 
+  const cellDiv = document.createElement("div"); /*Création de la div pour créer un pixel*/
+  cellDiv.classList.add("cell"); /*Ajouter une classe "cell" sur cette div*/
+  cellDiv.style.backgroundColor = "rgb(210, 218, 226)"; /*Ajouter un background gris/*/
   invaderDiv.appendChild(cellDiv); /*Ajouter de la div class="cell" dans la div id="invader"*/
 }
 
@@ -34,6 +34,30 @@ for (const cell of cells) {
 }
 
 
+//ETAPE 3 = configuration du Formulaire 
+
+/*Ajouter un champ input pour choisir la taille de la grille*/
+const configForm = document.querySelector('.configuration'); /*Selectionner la div form*/
+
+const inputDiv = document.createElement('input'); /*Création de la div input*/
+inputDiv.classList.add('grid-size'); /*Ajouter une classe "inputGrid" sur cette div*/
+configForm.appendChild(inputDiv); /*Ajout de l'input dans le formulaire*/
+
+/*Ajoute un bouton pour valider*/
+const button = document.createElement('button'); /*Création de la div button*/
+button.classList.add('submit-form'); /*Ajouter une classe "inputGrid" sur cette div*/
+button.textContent = "Valider";
+configForm.appendChild(button); /*Ajout du bouton dans le formulaire*/
+
+/*Lorsqu'on soumet le formulaire on veut générer une nouvelle grille à la place de la grille actuelle, mais en tenant compte de la taille saisie dans le champ*/
+/*Evenement submit => nouvelle grille en prenant en compte la valeur saisie dans l'input*/
+
+
+
+
+
+/*Ajouter un champ input pour choisir la taille en Px de la cell*/
+// cellSize
 
 
 
