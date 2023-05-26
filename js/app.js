862985ccf281eb70cf98579750cb29788aed0308
Elisabeth Faujour <efaujour@gmail.com>
2023-05-26 11:48:46 +0200

    Etape 1,2,3 corrigées

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
-//ETAPE 1 FONCTON GENERER UNE GRILLE 
+/*----FONCTON EVENEMENT ClICK COULEUR----*/
 
+function handleCellClick (event) {
+  event.target.classList.toggle('black');
+}
+
+/*----FONCTON CREER UNE GRILLE----*/
 function createGrid(gridSize, cellSize) {
-  
+
   /*Selection de la div id=invader*/
   const invaderDiv = document.getElementById('invader');
 
-  /*Appliquer le CSS pour la grille grid*/
-  invaderDiv.style.display = 'grid';
+  /*Modifier la taille de la grille*/
   invaderDiv.style.gridTemplateColumns = `repeat(${gridSize}, ${cellSize}px)`;
   invaderDiv.style.gridTemplateRows = `repeat(${gridSize}, ${cellSize}px)`;
 
@@ -14,62 +18,59 @@ function createGrid(gridSize, cellSize) {
   invaderDiv.innerHTML = '';
 
   /*Boucle pour générer la grille*/
-  for (let i = 0; i < gridSize * gridSize; i++) { 
+  for (let i = 0; i < gridSize * gridSize; i++) {
     const cellDiv = document.createElement('div'); /*Création de la div pour créer un pixel*/
     cellDiv.classList.add('cell'); /*Ajouter une classe 'cell' sur cette div*/
-    cellDiv.style.backgroundColor = 'rgb(210, 218, 226)'; /*Background gris/*/
     cellDiv.style.width = `${cellSize}px`; /*Largeur de la cell*/
     cellDiv.style.height = `${cellSize}px`; /*Hauteur de la cell*/
+    cellDiv.addEventListener('click', handleCellClick);
     invaderDiv.appendChild(cellDiv); /*Ajouter de la div class='cell' dans la div id='invader'*/
-    cellDiv.addEventListener('click', changeCellColor);/*Ajouter de l'evenement clic*/
   }
 }
 
 createGrid(8, 25);
 
 
-//ETAPE 3 CONFIG DU FORMULAIRE
+/*----CONFIG DU FORMULAIRE----*/
 
 /*Selectionner le formulaire*/
-const configForm = document.querySelector('.configuration'); /*Selectionner la div form*/
+const formConfig = document.querySelector('.configuration'); /*Selectionner la div form*/
 
-/*Créer 2 input et 1 bouton*/
-const gridSizeInput = document.createElement('input'); 
-const cellSizeInput = document.createElement('input'); 
-const button = document.createElement('button'); 
+/*Créer 2 input et 1 bouton* et ajouter leurs attributs types/id/name* avec la méthode setAttribute*/
+/****INPUT gridSize****/
+const gridSizeInput = document.createElement('input');
 
-/*Ajouter le type aux input*/
 gridSizeInput.setAttribute('type', 'number');
+gridSizeInput.setAttribute('name', 'gridSize');
+gridSizeInput.setAttribute('id', 'gridSize');
+gridSizeInput.setAttribute('placeholder', 'Taille de la grille');
+
+formConfig.appendChild(gridSizeInput);
+
+/****INPUT cellSize****/
+const cellSizeInput = document.createElement('input');
+
 cellSizeInput.setAttribute('type', 'number');
+cellSizeInput.setAttribute('name', 'cellSize');
+cellSizeInput.setAttribute('id', 'cellSize');
+cellSizeInput.setAttribute('placeholder', 'Taille d\'une cellule');
 
-/*Leur ajouter des class*/ 
-gridSizeInput.classList.add('grid-size'); 
-cellSizeInput.classList.add('cell-size'); 
-button.classList.add('submit-form'); 
+formConfig.appendChild(cellSizeInput);
 
-/*Ajouter du contenu au bouton*/ 
+/****BUTTON****/
+const button = document.createElement('button');
+
+button.setAttribute('type', 'submit');
+button.setAttribute('id', 'submitForm');
 button.textContent = 'Valider';
 
-/*Ajouter les div au formulaire*/
-configForm.appendChild(gridSizeInput); 
-configForm.appendChild(cellSizeInput); 
-configForm.appendChild(button);
+formConfig.appendChild(button);
 
 /*Evenement quand on submit le formulaire => générer une nouvelle grille en prenant en compte la valeur saisie dans l'input*/
 button.addEventListener('click', function (event) {
   event.preventDefault();
-  const gridValue = parseInt(document.querySelector('.grid-size').value);
-  const cellValue = parseInt(document.querySelector('.cell-size').value);
-  createGrid(gridValue,cellValue);
+  const gridValue = parseInt(document.getElementById('gridSize').value);
+  const cellValue = parseInt(document.getElementById('cellSize').value);
+  createGrid(gridValue, cellValue);
 });
 
-//ETAPE 2 FONCTION CHANGER LA COULEUR LORS DU CLIC
-
-function changeCellColor(event) {
-  const backgroundColor = event.target.style.backgroundColor;
-  if (backgroundColor === 'rgb(210, 218, 226)') {
-    event.target.style.backgroundColor = 'black';
-  } else {
-    event.target.style.backgroundColor = 'rgb(210, 218, 226)';
-  }
-}
